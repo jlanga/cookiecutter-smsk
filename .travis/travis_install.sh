7d2612a3cc38b07e9565784c338cc87acdd1ab89
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-02-15 15:39:27 +0100

    Adding channels for snakemake and cookiecutter

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 export PATH="$HOME/miniconda/bin:$PATH"
-conda install --yes cookiecutter snakemake
+conda install --yes -c conda-forge cookiecutter
+conda install --yes -c bioconda snakemake
