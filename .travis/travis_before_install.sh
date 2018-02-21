665a6143bf683de0b5b448ad1be2d567ee3bbe8b
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-02-21 18:10:05 +0100

    will it work?

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -3,16 +3,12 @@
 # Install miniconda
 ## http://conda-test.pydata.org/docs/travis.html
 
-if [[ -d $HOME/miniconda ]]; then
+if [[ -d "$HOME/miniconda" ]]; then
     echo "miniconda3 already installed."
 else
     # We do this conditionally because it saves us some downloading if the
     # version is the same.
-    if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
-    else
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-    fi
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     bash miniconda.sh -b -p $HOME/miniconda
     export PATH="$HOME/miniconda/bin:$PATH"
     hash -r
